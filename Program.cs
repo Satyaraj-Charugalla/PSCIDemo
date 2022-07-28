f7e2473687c6d81303800603262baf834deea5e8
Satyaraj-Charugalla <1210316950@gitam.in>
2022-07-28 06:44:45 +0000

    Made changes to the forked repo

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace PSCIDemo
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                  webBuilder.UseStartup<Startup>();
                 });
     }
 }
